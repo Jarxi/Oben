164c1397d1e0bee36345c1d820b6a463748ba25b
Qiusi Li <qiusili@usc.edu>
2019-09-29 20:52:22 -0700

    feat: fix login

--- client/src/Components/SignInForm.js
+++ client/src/Components/SignInForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import axios from 'axios'
 
 class SignInForm extends React.Component {
@@ -8,7 +8,9 @@ class SignInForm extends React.Component {
         
         this.state = {
             username: '',
-            password: ''
+            password: '',
+            loginfail:false
+
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -32,26 +34,48 @@ class SignInForm extends React.Component {
         const params = {
             email: this.state.username,
             password: this.state.password
-        }
+        };
+        axios.post(url,params).then((res)=>{
+            console.log(res)
+            if(res.status == 200){
+                this.setState({
+                    loginfail: false
+                })
+                alert("Log In Success")
+            }
+        }).catch((e)=>{
+            console.log("Sign in failed")
+            this.setState({
+                loginfail: true
+            })
+        })
+
     }
 
     render(){
+        let banner;
+        if(this.state.loginfail){
+            banner = <div class="Banner">Invalid email or password</div>
+        }
         return (
             <form className="FormFields" onSubmit={this.handleSubmit}>
                 <Link to="/signup"><button className="Signup_button">Sign Up</button></Link>
-                <div className="FormFiled">
+                <div className="FormField">
                     <div class="circle">Or</div>
                 </div>
-                <div className="FormFiled">
+                <div className="FormField">
                     <label className="FormField__Label" htmlFor="username">Email Address</label>
                     <input type="text" id="username" className="FormField__Input" placeholder="Enter your email address" 
                     name="username" value={this.state.email} onChange={this.handleChange}/>
                 </div>
-                <div className="FormFiled">
+                
+                <div className="FormField">
                     <label className="FormField__Label" htmlFor="password">Password</label>
                     <input type="password" id="password" className="FormField__Input" placeholder="Enter your pasword" 
                     name="password" value={this.state.password} onChange={this.handleChange}/>
                 </div> 
+
+                {banner}
                 
                 <div className="FormField">
                     <button className="FormField__Button">Log in</button>
--- client/src/Components/SignUpForm.js
+++ client/src/Components/SignUpForm.js
@@ -13,7 +13,8 @@ class SignInForm extends React.Component {
             firstname:'',
             lastname:'',
             password:'',
-            confirmPassword:''
+            confirmPassword:'',
+            success:false
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -33,54 +34,73 @@ class SignInForm extends React.Component {
     handleSubmit(e){
         e.preventDefault();
         const url = "http://localhost:3000/api/user/signup";
+        console.log(this.state.email)
         const params = {
-            email: this.state.email,
+            email: this.state.username,
             password: this.state.password,
             first_name: this.state.firstname,
             last_name: this.state.lastname,
-            user_type: "Admin"
+            user_type: "aic"
         }
         axios.post(url, params).then((res)=>{
-            if(res.data.code == 200){
-                console.log("Registeration Success")
+            if(res.status == 200){
+                this.setState({
+                    success:true
+                })
             }
+        }).catch((err)=>{
+            console.log("failed")
         })
+        
     }
 
     render(){
+        const signUpSuccess = this.state.success;
+        let button;
+        if(!signUpSuccess){
+            button = (
+                <div className="FormField">
+                        <button className="SignUpForm__Button">Sign Up</button>
+                </div>
+            );
+        }
+        else{
+            button = (
+                <div className="Succeed_message">Sign up succeed. Please check for a confirmation email</div>
+            )
+        }
         return (
             
             <div className="Container">
                 
                 <form className="SignUpBox" onSubmit={this.handleSubmit}>
-                    <div className="FormFiled">
+                    <div>
                         <input type="text" id="username" className="SignUpField__Input" placeholder="Email" 
                         name="username" value={this.state.username} onChange={this.handleChange}/>
                     </div>
 
-                    <div className="FormFiled">
+                    <div>
                         <input type="text" id="firstname" className="SignUpField__Input" placeholder="First Name" 
                         name="firstname" value={this.state.firstname} onChange={this.handleChange}/>
                     </div>
 
-                    <div className="FormFiled">
+                    <div>
                         <input type="text" id="lastname" className="SignUpField__Input" placeholder="Last Name" 
                         name="lastname" value={this.state.lastname} onChange={this.handleChange}/>
                     </div>
 
-                    <div className="FormFiled">
+                    <div>
                         <input type="password" id="password" className="SignUpField__Input" placeholder="Pasword" 
                         name="password" value={this.state.password} onChange={this.handleChange}/>
                     </div>
 
-                    <div className="FormFiled">
+                    <div>
                         <input type="password" id="confirmPassword" className="SignUpField__Input" placeholder="Retype pasword" 
                         name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChange}/>
                     </div> 
-
-                    <div className="FormField">
-                        <button className="SignUpForm__Button">Sign Up</button>
-                    </div>
+                    
+                    
+                    {button}
                 
                 </form>
             </div>
