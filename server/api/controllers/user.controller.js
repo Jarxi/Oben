51875f39b2a43cbfb54a0a8e137aeaa7a8566d19
Ruoxi Jia <ruoxijia@usc.edu>
2019-10-06 16:24:53 -0700

    reset password node

--- server/api/controllers/user.controller.js
+++ server/api/controllers/user.controller.js
@@ -10,7 +10,6 @@ const signUp = async (req, res) => {
         const user_data = req.body;
         const password = await encryptPassword(user_data.password);
         user_data.password = password.password;
-        console.log(user_data.password);
         const user = await User.create(user_data);
         return res.status(200).json({
             message: "user created!",
@@ -55,9 +54,51 @@ const signIn = async (req, res) => {
     }
 }
 
+const resetPassword = async (req, res) => {
+    try{
+        const user_data = req.body;
+        const email = req.body.email;
+        const new_password = req.body.new_password;
+        const encrypted_password = await encryptPassword(new_password);
+        const user = await User.findOne({
+            email
+        });
+
+        if (!user){
+            return sendErr(res, "Email does not exist");
+        }
+
+        const decrypted = await decryptPassword(user_data.password, user.password);
+        if (!decrypted.password) {
+            return sendErr(res, '', 'Email and Password do not match');
+        }
+
+        const update = await User.findOneAndUpdate({
+                email: email
+            }, {
+                $set: {
+                    password: encrypted_password.password
+                }
+            }
+        );
+
+        if (!update){
+            sendErr(res, '', "Some error occurred tryint to update password");
+        }
+
+        return res.status(200).json({
+            message: `Password Resetted!`,
+            user
+        });
+    } catch (err){
+        return sendErr(res, err);
+    }
+}
+
 
 // if you add functions above, add it here too
 module.exports = {
     signUp,
-    signIn
+    signIn,
+    resetPassword
 };
--- server/api/routes/user.routes.js
+++ server/api/routes/user.routes.js
@@ -7,4 +7,5 @@ const router = express.Router();
 router.get("/test", (req, res) => res.json({ msg: "user Works" }));
 router.post("/signup", user.signUp);
 router.post("/signin", user.signIn);
+router.post("/resetPassword", user.resetPassword);
 module.exports = router;
