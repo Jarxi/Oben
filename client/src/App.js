2065f35c0c94ba9090b353b26bbe11d531901f6b
Qiusi Li <qiusili@usc.edu>
2019-10-27 20:38:32 -0700

    fix: redirect to login page when user is not logged in

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import SignInForm from './Components/SignInForm';
 import SignUpForm from './Components/SignUpForm';
 import HomePage from './Pages/HomePage';
 import ResetPasswordForm from './Components/ResetPasswordForm';
-import Dashboard from './Pages/Dashboard'
-import '../node_modules/react-bootstrap/dist/react-bootstrap.min.js'
+import PrivateRoute from '../src/Components/PrivateRoute';
+import '../node_modules/react-bootstrap/dist/react-bootstrap.min.js';
 
 function App() {
 
@@ -14,9 +14,9 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/signup" component={SignUpForm} />
-        <Route exact path="/login" component={SignInForm} />      
-        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/" component={SignInForm} />      
         <Route exact path="/resetPassword" component={ResetPasswordForm} />
+        <PrivateRoute exact path="/home" component={HomePage} />
       </Router>
     </div>
 
