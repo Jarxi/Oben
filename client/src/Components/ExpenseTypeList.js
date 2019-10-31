0d03b503ce8cf4363f89a8f142d8b29970fb2187
陆逸淳 <yichunlu@usc-guestwireless-upc-newsc7286.usc.edu>
2019-10-31 14:09:01 -0700

    Build: Get Call for expense Category

--- client/src/Components/ExpenseTypeList.js
+++ client/src/Components/ExpenseTypeList.js
@@ -4,20 +4,52 @@ import '../CSS/EditTeamMember.css'
 import '../CSS/bootstrap/css/bootstrap-iso.css';
 
 class ExpenseTypeList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            error:null,
+            isLoaded: false,
+            expenseType:[]
+        };
+    }
 
+    componentDidMount(){
+        fetch("http://localhost:3000/api/expense/category")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    items: result.expenseType
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded:true,
+                    error
+                });
+                console.log("error occured")
+            }
+        )
+    }
     render(){
         // Hardcoded users info format!
         // TODO: Backend API return registered User Info
-        const ExpenseType = [
-            {name: 'Travel', email: 'qqwwerr@gmail.com'},
-            {name: 'Breakfast', email: '3432f@gmail.com'},
-        ];
-
-        return(
-            <ul id="EmployeeList" class="list-group bootstrap-iso">
-                {ExpenseType.map(expense => <li class="list-group-item list-group-item-light"><a href="#" >{expense.name}</a></li>)}
-            </ul>
-        )
+        // const ExpenseType = [
+        //     {name: 'Travel', email: 'qqwwerr@gmail.com'},
+        //     {name: 'Breakfast', email: '3432f@gmail.com'},
+        // ];
+        const {error, isLoaded, expenseType} = this.state;
+        if(error){
+            return <div>Error: {error.message}</div>;
+        }else if (!isLoaded){
+            return <div>Loading...</div>
+        }else{
+            return(
+                <ul id="EmployeeList" class="list-group bootstrap-iso">
+                    {expenseType.map(expense => <li class="list-group-item list-group-item-light"><a href="#" >{expense.name}</a></li>)}
+                </ul>
+            )}
     }
 }
 
