0b233426610eaf75d4be92e89d34ee2557e35dbf
Jessie Jia <ruoxijia@usc.edu>
2019-11-17 13:29:02 -0800

    approval page

--- client/src/Components/ApprovalTable.js
+++ client/src/Components/ApprovalTable.js
@@ -35,6 +35,7 @@ class ApprovalTable extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+
   isFloat(value) {
     return !isNaN(value) &&
         parseFloat(Number(value)) == value &&
@@ -190,14 +191,10 @@ class ApprovalTable extends React.Component {
     const expenseFirstDay = moment(firstDay);
     return (
         <div>
-          <div class="submissionSection bootstrap-iso">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" />
-              <label class="form-check-label">
-                Same as last week
-              </label>
-            </div>
+          <div className='outer_box'>
+            <p className='title'>Time Sheet Approval</p>
 
+            <div class="submissionSection bootstrap-iso">
             <Table bordered>
               <thead>
               <tr>
@@ -236,68 +233,111 @@ class ApprovalTable extends React.Component {
               </tr>
               </tbody>
             </Table>
-            <div className="submit_button">
-              <button type="button" class="btn btn-success" onClick={() => this.handleSubmit('timesheet')}>Submit</button>
-            </div>
+              <div className='row'>
+                <div className='buttons'>
+                  <button type="button" className="btn btn-success col">Approve</button>
+                  <button type="button" className="btn btn-danger col" >Return</button></div>
+              </div>
             <div className="error_message">
               <p> {this.state.timesheet_error}</p>
             </div>
           </div>
 
-          <div className="submissionSection bootstrap-iso">
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" value=""/>
-              <label className="form-check-label">
-                Same as last week
-              </label>
-            </div>
-
-            <Table bordered>
-              <thead>
-              <tr>
-                <td className="information">Ticket ID</td>
-                <td className="information">Input Expense</td>
-                <td className="date">{expenseFirstDay.date()}</td>
-                <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
-                <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
-                <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
-                <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
-                <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
-                <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
-              </tr>
-              </thead>
-              <tbody>
-              {this.state.expense_ticket_numbers.map(ticket_number => (
-                  <SubmissionRow onCellChange={this.onCellChange.bind(this, 'expense', ticket_number - 1)} ticket_number={ticket_number} key={ticket_number}/>
-              ))}
-              <tr>
-                <td>
-                  <IconButton onClick={this.addRow.bind(this, 'expense')} aria-label="delete" size="small">
-                    <AddCircle color='primary' fontSize="inherit"/>
-                  </IconButton>
-                  <IconButton onClick={this.deleteRow.bind(this, 'expense')} aria-label="delete" size="small">
-                    <Delete color='primary' fontSize="inherit"/>
-                  </IconButton>
-                </td>
-                <td>Total Expense</td>
-                <td>{this.state.expense_cols[0]}</td>
-                <td>{this.state.expense_cols[1]}</td>
-                <td>{this.state.expense_cols[2]}</td>
-                <td>{this.state.expense_cols[3]}</td>
-                <td>{this.state.expense_cols[4]}</td>
-                <td>{this.state.expense_cols[5]}</td>
-                <td>{this.state.expense_cols[6]}</td>
-              </tr>
-              </tbody>
-            </Table>
-            <div className="submit_button">
-              <button type="button" className="btn btn-success">Submit</button>
-            </div>
-            <div className="error_message">
-              <p>{this.state.expense_error}</p>
+          </div>
+          <div className='outer_box'>
+            <p className='title'>Expense Approval</p>
+            <div className="submissionSection bootstrap-iso">
+              <Table bordered>
+                <thead>
+                <tr>
+                  <td className="information">Ticket ID</td>
+                  <td className="information">Input Expense</td>
+                  <td className="date">{expenseFirstDay.date()}</td>
+                  <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
+                  <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
+                  <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
+                  <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
+                  <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
+                  <td className="date">{expenseFirstDay.add(1, 'day').date()}</td>
+                </tr>
+                </thead>
+                <tbody>
+                {this.state.expense_ticket_numbers.map(ticket_number => (
+                    <SubmissionRow onCellChange={this.onCellChange.bind(this, 'expense', ticket_number - 1)} ticket_number={ticket_number} key={ticket_number}/>
+                ))}
+                <tr>
+                  <td>
+                    <IconButton onClick={this.addRow.bind(this, 'expense')} aria-label="delete" size="small">
+                      <AddCircle color='primary' fontSize="inherit"/>
+                    </IconButton>
+                    <IconButton onClick={this.deleteRow.bind(this, 'expense')} aria-label="delete" size="small">
+                      <Delete color='primary' fontSize="inherit"/>
+                    </IconButton>
+                  </td>
+                  <td>Total Expense</td>
+                  <td>{this.state.expense_cols[0]}</td>
+                  <td>{this.state.expense_cols[1]}</td>
+                  <td>{this.state.expense_cols[2]}</td>
+                  <td>{this.state.expense_cols[3]}</td>
+                  <td>{this.state.expense_cols[4]}</td>
+                  <td>{this.state.expense_cols[5]}</td>
+                  <td>{this.state.expense_cols[6]}</td>
+                </tr>
+                </tbody>
+              </Table>
+              <div className='row'>
+                <div className='buttons'>
+                  <button type="button" className="btn btn-success col">Approve</button>
+                  <button type="button" className="btn btn-danger col" >Return</button></div>
+              </div>
+              <div className="error_message">
+                <p>{this.state.expense_error}</p>
+              </div>
             </div>
           </div>
 
+          <div className='outer_box'>
+            <p className='title'>Invoice Approval</p>
+            <div className="submissionSection bootstrap-iso">
+              <form className="form-inline">
+                <div className="form-group row col-sm-6">
+                  <label htmlFor="month" className="col-sm-3 col-form-label ">Month</label>
+                  <div className="col-sm-3">
+                    <input type="text" className="form-control small-input" id="month"/>
+                  </div>
+                </div>
+                <div className="form-group row col-sm-6">
+                  <label htmlFor="invoice_no" className="col-sm-4 col-form-label">Invoice Number</label>
+                  <div className="col-sm-2">
+                    <input type="text" className="form-control small-input" id="invoice_no"/>
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label htmlFor="total_days" className='col-sm-6 col-form-label'>Total Days in the invoice:</label>
+                  <input type="text" className="form-control col-sm-6" id="total_dyas"/>
+                </div>
+
+                <div className="form-group row">
+                  <label htmlFor="total_amount" className='col-sm-6 col-form-label'>Total Amount Submitted:</label>
+                  <input type="text" className="form-control col-sm-6" id="total_amount"/>
+                </div>
+
+                <div className="form-group row">
+                  <label htmlFor="service" className='col-sm-6 col-form-label'>Nature of Services Provided:</label>
+                  <input type="text" className="form-control col-sm-6" id="service"/>
+                </div>
+              </form>
+
+              <div className='row'>
+                <div className='buttons'>
+                  <button type="button" className="btn btn-success col">Approve</button>
+                  <button type="button" className="btn btn-danger col" >Return</button></div>
+              </div>
+              <div className="error_message">
+                <p>{this.state.expense_error}</p>
+              </div>
+            </div>
+          </div>
         </div>
 
 
