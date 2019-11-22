187981173f54db8727f3fe835427cfd5434e4ba3
Jessie Jia <ruoxijia@usc.edu>
2019-11-22 12:40:21 -0800

    contractor submission add log

--- client/src/Components/ApprovalLog.js
+++ client/src/Components/ApprovalLog.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import moment from 'moment';
 import '../CSS/bootstrap/css/bootstrap-iso.css';
-import ApprovalLogRow from "./ApprovalLogRow";
-import {Table} from "react-bootstrap";
-
-
+import ApprovalLogRow from './ApprovalLogRow';
+import { Table } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 class ApprovalLog extends React.Component {
   constructor(props) {
     super(props);
@@ -13,39 +12,50 @@ class ApprovalLog extends React.Component {
         {
           id: '123123123123123',
           name: 'abc',
-          dateType: moment().format('MM/DD').toString() + " Invoice",
+          dateType:
+            moment()
+              .format('MM/DD')
+              .toString() + ' Invoice',
           status: 'submitted'
         }
       ]
-    }
+    };
   }
 
-
-  render(){
-
+  render() {
     return (
-        <div className='outside_box'>
-          <span style={{color:'#4651af'}}>Approval Log</span>
-          <div class = "box bootstrap-iso">
-              <Table>
-                <thead>
-                <tr>
-                  <td className="name">Name</td>
-                  <td className="dateType"></td>
-                  <td className="status">Status</td>
-                </tr>
-                </thead>
-                <tbody>
-                {this.state.submissions.map(submission => (
-                    <ApprovalLogRow name={submission.name} dateType={submission.dateType} status={submission.status} key={submission.id}/>
-                ))}
-                </tbody>
-              </Table>
-          </div>
+      <div className='outside_box'>
+        <span style={{ color: '#4651af' }}>
+          {this.props.page === 'contractor' ? 'Status' : 'Approval Log'}
+        </span>
+        <div class='box bootstrap-iso'>
+          <Table>
+            <thead>
+              <tr>
+                <td className='name'>Name</td>
+                <td className='dateType'></td>
+                <td className='status'>Status</td>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.submissions.map(submission => (
+                <ApprovalLogRow
+                  name={submission.name}
+                  dateType={submission.dateType}
+                  status={submission.status}
+                  key={submission.id}
+                />
+              ))}
+            </tbody>
+          </Table>
         </div>
-
+      </div>
     );
   }
+}
+
+ApprovalLog.propTypes = {
+  page: PropTypes.string.isRequired
 };
 
 export default ApprovalLog;
--- client/src/Pages/SubmissionPage.js
+++ client/src/Pages/SubmissionPage.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
 import moment from 'moment';
 import Sidebar from '../Components/Sidebar';
-import SubmissionTable from "../Components/SubmissionTable";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import "../CSS/SubmissionPage.css";
-
+import SubmissionTable from '../Components/SubmissionTable';
+import ApprovalLog from '../Components/ApprovalLog';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import '../CSS/SubmissionPage.css';
 class SubmissionPage extends React.Component {
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       startDate: new Date()
     };
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = date => {
@@ -22,21 +21,34 @@ class SubmissionPage extends React.Component {
     });
   };
 
-  render(){
+  render() {
     // console.log(sessionStorage);
     return (
-        <div class="row">
+      <div class='row'>
+        <div className='col left-col'>
           <div class='datePick'>
             <DatePicker
-                inline = {true}
-                selected={this.state.startDate}
-                onChange={this.handleChange}
+              inline={true}
+              selected={this.state.startDate}
+              onChange={this.handleChange}
             />
           </div>
-          <SubmissionTable firstDay={moment(this.state.startDate).startOf('week')}/>
+          <ApprovalLog
+            page={
+              sessionStorage.getItem('user_type') === 'contractor'
+                ? 'contractor'
+                : ''
+            }
+          />
         </div>
+        <div className='col'>
+          <SubmissionTable
+            firstDay={moment(this.state.startDate).startOf('week')}
+          />
+        </div>
+      </div>
     );
   }
-};
+}
 
 export default SubmissionPage;
