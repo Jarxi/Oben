8899fdecf3c02b531e5970922f2f1e4ccee308c0
Kun Peng <kunpeng@usc.edu>
2019-11-24 00:20:53 -0800

    Revert "Merge branch 'login_page' of https://github.com/Jarxi/Oben into login_page"
    This reverts commit 7ab2a5fd9ebebbbb4d84293a4b85b2955561d8ba, reversing
    changes made to 874bfdd8a32a844707bfbec2f210781e6ae99531.

--- client/src/Components/EditTeamMember.js
+++ client/src/Components/EditTeamMember.js
@@ -1,50 +1,28 @@
 import React from 'react';
 import EmployeeList from './EmployeeList';
 
-import '../CSS/EditTeamMember.css';
+import '../CSS/EditTeamMember.css'
 import '../CSS/bootstrap/css/bootstrap-iso.css';
-import AddCircle from '@material-ui/icons/Add';
-import Delete from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
+
+
 class EditTeamMember extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      teams: ['Engineering', 'Marketing', 'Oben']
-    };
-  }
-  render() {
+  render(){
+
     return (
-      <div class='canvas'>
-        <div className='title'>Create Team Names</div>
-        <div className='wrapper'>
-          {this.state.teams.map(team => (
-            <button className='teamName btn' key={team}>
-              {team}
-            </button>
-          ))}
-          <div>
-            <input type='text' placeholder='new team name' />
-          </div>
-          <div>
-            <IconButton aria-label='delete' size='small'>
-              {' '}
-              <AddCircle color='primary' fontSize='inherit' />
-            </IconButton>
-          </div>
-        </div>
-        <div class='employeeList'>
-          <p class='ListTitle'>Name of Employee</p>
-          <div class='ListWrapper'>
-            <EmployeeList />
-          </div>
-        </div>
-        <div class='UserInfo'>
-          <p class='ListTitle'>For Later Use to display UserInfo</p>
-        </div>
-      </div>
+        <div class="canvas">
+                <div class="employeeList">
+                    <p class="ListTitle">Name of Employee: </p>
+                    <div class="ListWrapper">
+                        <EmployeeList/>
+                    </div>
+
+                </div>
+                <div class="UserInfo">
+                    <p class="ListTitle">For Later Use to display UserInfo</p>
+                </div>
+            </div>
     );
   }
-}
+};
 
 export default EditTeamMember;
