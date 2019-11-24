c35b51f6a5bc30ad955c53fce9733b78e95babe5
Jessie Jia <ruoxijia@usc.edu>
2019-11-23 18:27:41 -0800

    getuserbyid

--- server/api/controllers/user.controller.js
+++ server/api/controllers/user.controller.js
@@ -4,75 +4,89 @@ const mongoose = require('mongoose');
 const { User } = require('../models');
 const sendErr = require('../utils/sendErr');
 
-
-
 const updateUserInfo = async (req, res) => {
-    try{
-        // TODO: Upload W4 file and generate filename
-        const w4_filename = '';
-        const id = req.body.employee_id;
-        const user = await User.findOne({
-            employee_id: id
-        });
-        if (!user){
-            return sendErr(res, 'Internal Error: Cannot find employee with id: ' + id);
-        }
-
-
-        const update = await User.findOneAndUpdate(
-            {
-                employee_id: req.body.employee_id
-            },
-            {
-                $set: {
-                    team: req.body.team,
-                    start_date: req.body.start_date,
-                    birthday: req.body.birthday,
-                    status: req.body.status,
-                    past_status: req.body.past_status,
-                    email: req.body.email,
-                    phone: req.body.phone,
-                    legal_status: req.body.legal_status,
-                    visa_expiration: req.body.visa_expiration,
-                    insurance: req.body.insurance,
-                    dental: req.body.dental,
-                    vision: req.body.vision,
-                    leader: req.body.leader,
-                    team_led: req.body.team_led,
-                    approve_timesheet: req.body.approve_timesheet,
-                    approve_invoice: req.body.approve_invoice,
-                    w4: w4_filename,
-                }
-            }
-        );
-
-        if (!update){
-            sendErr(res, '', "Some error occurred trying to update user info");
-        }
-
-        return res.status(200).json({
-            message: `User info updated!`,
-            user
-        });
-    } catch (err){
-        return sendErr(res, err);
+  try {
+    // TODO: Upload W4 file and generate filename
+    const w4_filename = '';
+    const id = req.body.employee_id;
+    const user = await User.findOne({
+      employee_id: id
+    });
+    if (!user) {
+      return sendErr(
+        res,
+        'Internal Error: Cannot find employee with id: ' + id
+      );
     }
+
+    const update = await User.findOneAndUpdate(
+      {
+        employee_id: req.body.employee_id
+      },
+      {
+        $set: {
+          team: req.body.team,
+          start_date: req.body.start_date,
+          birthday: req.body.birthday,
+          status: req.body.status,
+          past_status: req.body.past_status,
+          email: req.body.email,
+          phone: req.body.phone,
+          legal_status: req.body.legal_status,
+          visa_expiration: req.body.visa_expiration,
+          insurance: req.body.insurance,
+          dental: req.body.dental,
+          vision: req.body.vision,
+          leader: req.body.leader,
+          team_led: req.body.team_led,
+          approve_timesheet: req.body.approve_timesheet,
+          approve_invoice: req.body.approve_invoice,
+          w4: w4_filename
+        }
+      }
+    );
+
+    if (!update) {
+      sendErr(res, '', 'Some error occurred trying to update user info');
+    }
+
+    return res.status(200).json({
+      message: `User info updated!`,
+      user
+    });
+  } catch (err) {
+    return sendErr(res, err);
+  }
 };
 
 const getUsers = async (req, res) => {
-    try{
-        const users = await User.find({});
-        return res.status(200).json({
-           message: 'All users',
-           users
-        });
-    }catch(err){
-        console.log(err);
-        sendErr(res, err)
-    }
+  try {
+    const users = await User.find({});
+    return res.status(200).json({
+      message: 'All users',
+      users
+    });
+  } catch (err) {
+    console.log(err);
+    sendErr(res, err);
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.find({ _id: req.userId });
+    return res.status(200).json({
+      message: 'User is found',
+      user
+    });
+  } catch (err) {
+    console.log(err);
+    sendErr(res, err);
+  }
 };
 // if you add functions above, add it here too
 module.exports = {
-    updateUserInfo,
-    getUsers,
+  updateUserInfo,
+  getUsers,
+  getUserById
 };
--- server/api/routes/user.routes.js
+++ server/api/routes/user.routes.js
@@ -8,8 +8,8 @@ const router = express.Router();
 router.use(authUtil.verifyToken);
 router.use(authUtil.isLoggedIn);
 
-
-router.get("/test", (req, res) => res.json({ msg: "user Works" }));
-router.put("/userInfo", user.updateUserInfo);
-router.get("/users", user.getUsers);
+router.get('/test', (req, res) => res.json({ msg: 'user Works' }));
+router.put('/userInfo', user.updateUserInfo);
+router.get('/users', user.getUsers);
+router.get('/', user.getUserById);
 module.exports = router;
