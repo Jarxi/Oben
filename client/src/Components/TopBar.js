0b41e7577da976ec804c3d2d37e8b73f13b40033
Jessie Jia <ruoxijia@usc.edu>
2019-11-25 01:43:49 -0800

    style

--- client/src/Components/TopBar.js
+++ client/src/Components/TopBar.js
@@ -24,28 +24,6 @@ class TopBar extends React.Component {
                   : sessionStorage.getItem('user_type')}
               </p>
             </div>
-            {this.props.location.pathname === '/home/override' && (
-              <div className='chooseTeamAndMember'>
-                <select>
-                  <option selected='selected'>Select Team</option>
-                  <option value='volvo'>1</option>
-                  <option value='saab'>2</option>
-                  <option value='mercedes'>3</option>
-                  <option value='audi'>4</option>
-                </select>
-              </div>
-            )}
-            {this.props.location.pathname === '/home/override' && (
-              <div className='chooseTeamAndMember'>
-                <select>
-                  <option selected='selected'>Select Team Member</option>
-                  <option value='volvo'>1</option>
-                  <option value='saab'>2</option>
-                  <option value='mercedes'>3</option>
-                  <option value='audi'>4</option>
-                </select>
-              </div>
-            )}
           </div>
         </div>
       </div>
--- client/src/Pages/OverridePage.js
+++ client/src/Pages/OverridePage.js
@@ -14,85 +14,101 @@ class OverridePage extends React.Component {
     this.state = {
       startDate: new Date(),
       currUserSubmissions: [],
-      userList:[],
-      selectedUserName:''
+      userList: [],
+      selectedUserName: ''
     };
-    this.getSubmissionBySelectedUser = this.getSubmissionBySelectedUser.bind(this);
+    this.getSubmissionBySelectedUser = this.getSubmissionBySelectedUser.bind(
+      this
+    );
     this.handleSelect = this.handleSelect.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.fetchSubmissions();
-        const config = {
-            headers:{            
-                authorization: "Bearer " + sessionStorage.getItem('token')
-            }
-        };
-        axios.get("http://localhost:3000/api/user/users",config)
-        .then(
-            (res) => {
-                console.log(res)
-                let user = res.data.users;
-                this.setState({
-                    userList: user
-                });
-
-            },
-            (error) => {
-                console.log(error);
-            }
-            )
-  }
-
-  getSubmissionBySelectedUser(){
-    alert(this.state.selectedUserName)
-    const res = this.state.allSubmissions.filter(submission => submission.submitter_name === this.state.selectedUserName)
-    this.setState({
-      currUserSubmissions: [...res]
-    })
-  }
-  
-  handleSelect(e){
-    this.setState({
-        selectedUserName: e.target.value
-    },()=>this.getSubmissionBySelectedUser())
-  }
-
-  fetchSubmissions(){
-    const url = "http://localhost:3000/api/submission/getAll";
-    const options = {
-      headers: { 
-        authorization: 'Bearer ' + sessionStorage.getItem('token'),
+    const config = {
+      headers: {
+        authorization: 'Bearer ' + sessionStorage.getItem('token')
       }
     };
-    axios.get(url, options).then((res)=>{
-        if(res.status === 200){
-            console.log(res.data.submissions)
-            this.setState({
-              allSubmissions: res.data.submissions,
-              isfetching: false,
-            }, console.log("FETCH COMPLETE", this.state.allSubmissions))
-        }
-    }).catch((e)=>{
-        console.log(e)
-        console.log('Get all submissions failed')
+    axios.get('http://localhost:3000/api/user/users', config).then(
+      res => {
+        console.log(res);
+        let user = res.data.users;
+        this.setState({
+          userList: user
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getSubmissionBySelectedUser() {
+    alert(this.state.selectedUserName);
+    const res = this.state.allSubmissions.filter(
+      submission => submission.submitter_name === this.state.selectedUserName
+    );
+    this.setState({
+      currUserSubmissions: [...res]
     });
+  }
+
+  handleSelect(e) {
+    this.setState(
+      {
+        selectedUserName: e.target.value
+      },
+      () => this.getSubmissionBySelectedUser()
+    );
+  }
+
+  fetchSubmissions() {
+    const url = 'http://localhost:3000/api/submission/getAll';
+    const options = {
+      headers: {
+        authorization: 'Bearer ' + sessionStorage.getItem('token')
+      }
+    };
+    axios
+      .get(url, options)
+      .then(res => {
+        if (res.status === 200) {
+          console.log(res.data.submissions);
+          this.setState(
+            {
+              allSubmissions: res.data.submissions,
+              isfetching: false
+            },
+            console.log('FETCH COMPLETE', this.state.allSubmissions)
+          );
+        }
+      })
+      .catch(e => {
+        console.log(e);
+        console.log('Get all submissions failed');
+      });
   }
 
   render() {
     return (
       <div class='row'>
-        <div className='chooseTeamAndMember'>
-                <select onChange={this.handleSelect}>
-                  <option selected='selected'>Select Team Member</option>
-                  {this.state.userList.map(user => <option value={user.first_name + " " + user.last_name}>{user.first_name + " " + user.last_name}</option>)}
-                </select>
-        </div>
         <div class='datePick'>
+          <div>
+            <select onChange={this.handleSelect}>
+              <option selected='selected'>Select Team Member</option>
+              {this.state.userList.map(user => (
+                <option value={user.first_name + ' ' + user.last_name}>
+                  {user.first_name + ' ' + user.last_name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <br></br>
           <DatePicker
             inline={true}
             selected={this.state.startDate}
-            onChange={(date)=>this.setState({startDate:date})}
+            onChange={date => this.setState({ startDate: date })}
           />
         </div>
         <OverrideTable
